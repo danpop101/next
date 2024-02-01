47815e74246adb742dba8b030a9b40bb63f5afb2
Daniel Pop <dpop100@gmail.com>
2024-02-01 10:43:47 +0000

    auth

--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -1,4 +1,6 @@
-import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+"use client";
+
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
 
