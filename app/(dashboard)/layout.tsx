acdd3807aa4a2e2eeacb49b8cb0821ef9635da8c
Daniel Pop <dpop100@gmail.com>
2024-02-01 12:30:57 +0000

    feat: Update global styles, add sidebar component, add dashboard layout component, add dashboard page component and add loading component

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,18 @@
+import { Sidebar } from "./_components/sidebar";
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function DashboardLayout({ 
+    children
+ }: DashboardLayoutProps) {
+  return (
+    <main className="h-full">
+        <Sidebar/>
+        <div className="pl-[60px] h-full">
+            {children}
+        </div>
+    </main>
+);
+}
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,14 @@
+import { UserButton } from "@clerk/nextjs"
+
+export default function DashboardPage() {
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div>
+        <h1>Dashboard Page</h1>
+      </div>
+    <div>
+      <UserButton />
+    </div>
+    </div>
+  );
+}
