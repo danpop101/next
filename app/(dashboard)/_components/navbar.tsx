9f4b54149b8bfc2dd2871f5515cbd350b770f215
Daniel Pop <dpop100@gmail.com>
2024-02-04 16:52:11 +0000

    style: Update navbar styling in dashboard

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -2,7 +2,7 @@ import { UserButton } from "@clerk/nextjs"
 
 export const Navbar = () => {
  return (
-   <div className="mx-10 flex justify-between px-4">
+   <div className="mx-10 flex justify-between items-center">
     <div>test</div>
     <UserButton/>
    </div>
